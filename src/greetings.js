bfedd371b6a1136d72832d76559750179bf6f1fa
Tammy Higgins <tmhiggins1986@gmail.com>
2025-04-22 19:21:27 -0500

    added

--- src/greetings.js
+++ src/greetings.js
@@ -1,1 +1,9 @@
 'use strict'
+
+export function sayHello(Tammy) {
+  return `Hello, ${Tammy}!`;
+}
+
+export function sayGoodbye(Tammy) {
+  return `Goodbye, ${Tammy}!`;
+}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 'use strict'
 
+import { sayHello, sayGoodbye } from "./greetings.js";
+
+console.log(sayHello("Webpack"));
+console.log(sayGoodbye("Webpack"));
